a397732077e04fb615aa78dd3e2e9c872652472d
Brandt Strom <brandtkstrom@gmail.com>
2019-10-03 15:50:11 -0700

    begin html and api route definitions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,14 @@
+const db = require('../models');
 
 module.exports = app => {
-    // TODO - add api routes
-}
+    app.get('/api/articles', (req, res) => {
+        // Get all articles from DB
+
+        db.Headline.find()
+            .then(docs => {
+                console.log('endpoint hit');
+                res.json(docs);
+            })
+            .catch(err => res.status(500).send(err));
+    });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,31 @@
+const DB = require('../models');
+const Scraper = require('../app/scraper');
 
 module.exports = app => {
-    // TODO - add html routes
-}
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+
+    app.get('/saved', (req, res) => {
+        // Get all saved headlines from DB
+        DB.Headline.find()
+            .then(docs => {
+                res.render('saved');
+            })
+            .catch(error => {
+                res.status(500).send(error.message || error);
+            });
+    });
+
+    app.get('/scrape', (req, res) => {
+        // Scrape new headlines, add to DB, then render view
+        const scraper = new Scraper();
+        scraper.scrapeNews(3).then(headlines => {
+
+
+            res.render('home', headlines);
+        }).catch(error => {
+            res.status(500).send(error.message || error);
+        })
+    })
+};
