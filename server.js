5a08c8e3604513952fde33fd7f85504b746d56c0
Brandt Strom <brandtkstrom@gmail.com>
2019-10-04 00:06:20 -0700

    rename files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const DB = require('./app/db');
 
 // Init the Express app
-const app = require('./app/server');
+const app = require('./app/express');
 
 // Start server once connected to DB
 const PORT = process.env.PORT || 3000;
@@ -10,3 +10,7 @@ DB.once('open', () => {
     console.log('Successfully connected to MongoDB!');
     app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 });
+
+// const Scraper = require('./app/scraper');
+// const scraper = new Scraper();
+// scraper.scrapeNews();
